af675ddc55f82ddda7503cf8f6ffc199f56ad842
Yoma <codeyoma@gmail.com>
2022-07-11 12:00:23 +0900

    edit megaphone

--- circle_04/CPP_Module/CPP_Module_00/ex00/megaphone.cpp
+++ circle_04/CPP_Module/CPP_Module_00/ex00/megaphone.cpp
@@ -1,28 +1,21 @@
 #include <cctype>
 #include <iostream>
 
-/*
-#define MEGAPHONE_DEF_MSG "* LOUD AND UNBEARABLE FEEDBACK NOISE *"
-
 int main(int argc, char* argv[])
 {
     std::string str;
+
     if (argc > 1)
     {
-        for (int i = 1; i < argc; i++)
-        {
-            const std::string arg(argv[i]);
-            for (std::size_t j = 0; j < arg.length(); j++)
-            {
-                str += std::toupper(arg[j]);
-            }
-        }
-    }
+		for (int i = 1; i < argc; i++)
+		{
+			std::string	temp = argv[i];
+			for (std::size_t j = 0; j < temp.length(); j++)
+				str += std::toupper(temp[j]);
+		}
+	}
     else
-    {
-        str = MEGAPHONE_DEF_MSG;
-    }
+        str = "* LOUD AND UNBEARABLE FEEDBACK NOISE *";
     std::cout << str << std::endl;
     return 0;
 }
-*/
